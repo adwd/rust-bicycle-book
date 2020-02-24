2a618260875977f0a172127afa91c8e0f3dc3fad
adwd <ecdt.mn@gmail.com>
2020-02-24 21:48:39 +0900

    add toy-vec

---
+++ toy-vec/src/lib.rs
@@ -0,0 +1,49 @@
+pub struct ToyVec<T> {
+    elements: Box<[T]>,
+    len: usize,
+}
+
+impl<T: Default> ToyVec<T> {
+    pub fn new() -> Self {
+        Self::with_capacity(0)
+    }
+
+    pub fn with_capacity(capacity: usize) -> Self {
+        Self {
+            elements: Self::allocate_in_heap(capacity),
+            len: 0,
+        }
+    }
+
+    fn allocate_in_heap(size: usize) -> Box<[T]> {
+        std::iter::repeat_with(Default::default)
+            .take(size)
+            .collect::<Vec<_>>()
+            .into_boxed_slice()
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    pub fn capacity(&self) -> usize {
+        self.elements.len()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    use super::ToyVec;
+
+    #[test]
+    fn toy_vec_create() {
+        let t: ToyVec<i32> = ToyVec::new();
+        assert_eq!(t.len(), 0);
+        assert_eq!(t.capacity(), 0);
+    }
+}
